7fa7a9311a19f29cec50c5007de4550b148cc745
gattyan27 <radio.radirer@gmail.com>
2020-12-08 18:26:29 +0900

    change install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+sudo yum install -y zsh
+sudo yum install -y util-linux-user
+
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
